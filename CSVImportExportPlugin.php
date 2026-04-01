31c93e192b15319400bb2355519a3f1a0fc2c3f6
Guilherme Godoy <guilhermehlgodoy22@gmail.com>
2026-04-01 15:17:07 +0100

    feat(01-01): parse --dry-mode flag in plugin entry point
    - Add $dryMode local variable with array_search/unset/array_values pattern
    - Parse --dry-mode from CLI args following --sendWelcomeEmail pattern
    - Pass $dryMode to both PreprintCommand and UserCommand constructors

--- CSVImportExportPlugin.php
+++ CSVImportExportPlugin.php
@@ -107,6 +107,14 @@ class CSVImportExportPlugin extends ImportExportPlugin
             $args = array_values($args);
         }
 
+        $dryMode = false;
+        $key = array_search('--dry-mode', $args);
+        if ($key !== false) {
+            $dryMode = true;
+            unset($args[$key]);
+            $args = array_values($args);
+        }
+
         $this->command = array_shift($args);
         $this->username = array_shift($args);
         $this->sourceDir = array_shift($args);
@@ -124,8 +132,8 @@ class CSVImportExportPlugin extends ImportExportPlugin
         $this->validateUser();
 
         match ($this->command) {
-            'preprints' => (new PreprintCommand($this->sourceDir, $this->user))->run(),
-            'users' => (new UserCommand($this->sourceDir, $this->user, $this->sendWelcomeEmail))->run(),
+            'preprints' => (new PreprintCommand($this->sourceDir, $this->user, $dryMode))->run(),
+            'users' => (new UserCommand($this->sourceDir, $this->user, $this->sendWelcomeEmail, $dryMode))->run(),
             default => throw new \InvalidArgumentException(__('plugins.importexport.csv.invalidCommand', ['command' => $this->command])),
         };
 
